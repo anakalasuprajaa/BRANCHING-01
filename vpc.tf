1b079d8cd00d9b586cf446df09478713804da589
anakalasuprajaa <anakalasuprajaa@gmail.com>
2026-04-01 12:26:29 +0530

    subnet block added successfully

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "vpc" {
     }
   
 }
+
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.1.1.0/24"
+    tags = {
+      Name = "Supraja-Subnet"
+    }
+  
+}
